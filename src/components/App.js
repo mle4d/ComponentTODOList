dd6506dddcac9b95b66e49ef0ac9e38f371c66d6
emily <mle4d@me.com>
2019-05-22 10:52:20 -0700

    header into appjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,18 @@
+import component from './component.js';
+import Header from './Header.js';
 
+class App extends component {
+
+    render() {
+        const dom = this.renderDOM();
+
+        const header = new Header();
+        const headerDOM = header.render();
+
+        const main = dom.querySelector('main');
+        dom.insertBefore(headerDOM, main);
+    }
+}
 
 
 export default App;
