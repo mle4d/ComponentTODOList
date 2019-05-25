4592a2031e110eab08d09802d04aac4af50b2d3e
emily <mle4d@me.com>
2019-05-24 17:11:21 -0700

    got tests to pass!

--- test/filter-todos.test.js
+++ test/filter-todos.test.js
@@ -10,7 +10,7 @@ const todos = [
     },
     {
         item: 'peas & carrots',
-        completed: false
+        completed: true
     },
     {
         item: 'flour',
@@ -33,4 +33,49 @@ test ('filter by item', assert => {
     ]);
     
 
+});
+
+test('not case sensitive', assert => {
+    const filter = {
+        item: 'chi',
+        completed: true
+    };
+
+    const filtered = filterTodos(todos, filter);
+
+    assert.deepEqual(filtered, [{
+        item: 'chikn',
+        completed: true
+    }]);
+});
+
+test('returns all on no filter', assert => {
+    const filter = {
+        item: '',
+        completed: ''
+    };
+
+    const filtered = filterTodos(todos, filter);
+
+    assert.deepEqual(filtered, todos);
+});
+
+test('completed and task', assert => {
+    const filter = {
+        item: 'c',
+        completed: true
+    };
+
+    const filtered = filterTodos(todos, filter);
+
+    assert.deepEqual(filtered, [
+        {
+            item: 'chikn',
+            completed: true
+        },
+        {
+            item: 'peas & carrots',
+            completed: true
+        }
+    ]);
 });
